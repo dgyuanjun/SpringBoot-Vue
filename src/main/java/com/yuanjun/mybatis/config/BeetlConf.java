318f0504f03029f7eceac10f8974847f1cb1326e
xiaobaishui <1872255070@qq.com>
2018-03-13 19:53:31 +0800

    1.添加beetl模板支持

---
+++ src/main/java/com/yuanjun/mybatis/config/BeetlConf.java
@@ -0,0 +1,48 @@
+package com.yuanjun.mybatis.config;
+
+import org.beetl.core.resource.WebAppResourceLoader;
+import org.beetl.ext.spring.BeetlGroupUtilConfiguration;
+import org.beetl.ext.spring.BeetlSpringViewResolver;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternUtils;
+
+import java.io.IOException;
+
+/**
+ * @author Administrator
+ * @create 2018/3/9-16:16
+ * @DESCRIPTION beetl系统配置
+ */
+@Configuration
+public class BeetlConf {
+    @Bean(initMethod = "init", name = "beetlConfig")
+    public BeetlGroupUtilConfiguration getBeetlGroupUtilConfiguration() {
+        BeetlGroupUtilConfiguration beetlGroupUtilConfiguration = new BeetlGroupUtilConfiguration();
+        ResourcePatternResolver patternResolver = ResourcePatternUtils.getResourcePatternResolver(new DefaultResourceLoader());
+        try {
+            // WebAppResourceLoader 配置root路径是关键
+            WebAppResourceLoader webAppResourceLoader =
+                    new WebAppResourceLoader(patternResolver.getResource("classpath:/").getFile().getPath());
+            beetlGroupUtilConfiguration.setResourceLoader(webAppResourceLoader);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //读取配置文件信息
+        return beetlGroupUtilConfiguration;
+    }
+
+    @Bean(name = "beetlViewResolver")
+    public BeetlSpringViewResolver getBeetlSpringViewResolver(@Qualifier("beetlConfig") BeetlGroupUtilConfiguration beetlGroupUtilConfiguration) {
+        BeetlSpringViewResolver beetlSpringViewResolver = new BeetlSpringViewResolver();
+        beetlSpringViewResolver.setPrefix("WEB-INF/views/");
+        beetlSpringViewResolver.setSuffix(".html");
+        beetlSpringViewResolver.setContentType("text/html;charset=UTF-8");
+        beetlSpringViewResolver.setOrder(0);
+        beetlSpringViewResolver.setConfig(beetlGroupUtilConfiguration);
+        return beetlSpringViewResolver;
+    }
+}
